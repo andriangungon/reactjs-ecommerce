b589f1f692cc9e4845ebf3ac8c4bb8e30fa3a3bd
dian <andriangungon@gmail.com>
2020-01-19 23:21:13 +0800

    added firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,10 @@
 import React  from 'react';
 
-import './sign-in.styles.scss';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
+import './sign-in.styles.scss';
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -51,6 +52,10 @@ class SignIn extends React.Component {
                     />                    
 
                     <CustomButton type="submit">Sign in</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>
+                        {''}
+                        Sign in with Google {''}
+                    </CustomButton>
                 </form>
             </div>
         )
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBNJz1Uxt6SZ2z6L1u6RHLdDcGcOfJLfGI",
+    authDomain: "ecommerce-b24da.firebaseapp.com",
+    databaseURL: "https://ecommerce-b24da.firebaseio.com",
+    projectId: "ecommerce-b24da",
+    storageBucket: "ecommerce-b24da.appspot.com",
+    messagingSenderId: "27599491126",
+    appId: "1:27599491126:web:f6e1229feff8b1d56e8897",
+    measurementId: "G-JSMT61BMGY"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
