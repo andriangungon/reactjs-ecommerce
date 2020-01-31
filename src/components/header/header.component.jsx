30497c60371272e2b8a4ce5f478f960963781ff6
dian <andriangungon@gmail.com>
2020-02-01 03:45:16 +0800

    implemented redux to cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser, dian }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>    
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -17,21 +17,20 @@ const Header = ({ currentUser, dian }) => (
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/shop'>CONTACT</Link>
-            <Link className='option' to='/sign-in'>SIGN IN</Link>         
-            {console.log('hi', dian)}     
+            <Link className='option' to='/sign-in'>SIGN IN</Link>                     
             {
                 currentUser ? ( <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> )
                 : ( <Link className='option' to='/sign-in'>SIGN IN</Link> )
             }
             <CartIcon />
         </div>
-        <CartDropdown />
+        { hidden ? null : <CartDropdown /> }        
     </div>
 )
 
-const mapStateToProps = state => ({    
-    currentUser: state.user.currentUser,
-    dian: state.user.dian    
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({    
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
