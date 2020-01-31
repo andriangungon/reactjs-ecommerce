d1158f6170c3e80a1cfa88ec527c1677699998b4
Dian <andriangungon@gmail.com>
2020-01-31 17:11:16 +0800

    started working on cart-icon components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
@@ -18,12 +20,12 @@ const Header = ({ currentUser, dian }) => (
             <Link className='option' to='/sign-in'>SIGN IN</Link>         
             {console.log('hi', dian)}     
             {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/sign-in'>SIGN IN</Link>
+                currentUser ? ( <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> )
+                : ( <Link className='option' to='/sign-in'>SIGN IN</Link> )
             }
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
